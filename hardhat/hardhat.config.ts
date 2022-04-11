014f15b1184a0c9d8ec1bde218eb6b82f240bb09
Mattia Pomelli <mattiapomelli@gmail.com>
2022-04-11 20:38:14 +0200

    Set up hardhat with typescript

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,18 @@
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-waffle'
+import { task, HardhatUserConfig } from 'hardhat/config'
+
+task('accounts', 'Prints the list of accounts', async (_, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.4',
+}
+
+export default config
---
+++ hardhat/scripts/deploy.ts
@@ -0,0 +1,21 @@
+import { ethers } from 'hardhat'
+
+async function main() {
+  const [owner] = await ethers.getSigners()
+
+  // Deploy contract
+  const Greeter = await ethers.getContractFactory('Greeter')
+  const greeter = await Greeter.deploy('Hello, Hardhat!')
+
+  await greeter.deployed()
+
+  console.log('Greeter deployed to:', greeter.address)
+  console.log('Deployed by:', owner)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
