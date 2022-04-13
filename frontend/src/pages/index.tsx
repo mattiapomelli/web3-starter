8280fb49be15b76ed3a7de7f43d32832c2d9d92f
Mattia Pomelli <mattiapomelli@gmail.com>
2022-04-13 19:12:35 +0200

    Add tailwind css and ethers

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,46 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-  return <div>Web3 App</div>
+  const [account, setAccount] = useState(null)
+
+  useEffect(() => {
+    const getConnectedAccount = async () => {
+      if (!window.ethereum) return
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' })
+
+      if (accounts.length !== 0) {
+        setAccount(accounts[0])
+      }
+    }
+
+    getConnectedAccount()
+  }, [])
+
+  const connectWallet = async () => {
+    try {
+      if (!window.ethereum) return
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+
+      setAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+      <button
+        onClick={connectWallet}
+        className="py-2 px-4 bg-blue-500 text-white rounded-full"
+      >
+        Connect
+      </button>
+      <h1>{account}</h1>
+    </div>
+  )
 }
 
 export default Home
